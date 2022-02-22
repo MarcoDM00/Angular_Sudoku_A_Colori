a71f024259d7a618668ae81acffa027e88b51152
MarcoDM00 <demattia.marco@iisgalvanimi.edu.it>
2022-02-22 18:32:04 +0000

    home_generale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angular_Sudoku_A_Colori';
+export class AppComponent implements OnInit {
+  coloreSfondo:string = "white";
+  
+  ngOnInit(): void {
+      
+  }
+  
+  settings() {
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import { SinglePlayerComponent } from './single-player/single-player.component';
+import { MultiPlayerComponent } from './multi-player/multi-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SinglePlayerComponent,
+    MultiPlayerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
