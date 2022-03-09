22d63094adc5596daff816aba34aac53ec75d48a
MarcoDM00 <demattia.marco@iisgalvanimi.edu.it>
2022-03-09 08:33:44 +0000

    3456

--- src/app/single-player/single-player.component.ts
+++ src/app/single-player/single-player.component.ts
@@ -8,9 +8,19 @@ import { Settings } from '../settings';
 })
 export class SinglePlayerComponent {
   caselle:{mostra:boolean, testo:string, bckColor:string}[] = [];
+  valori:{mostra:boolean, testo:string}[] = [];
+  valore:string = "";
 
   constructor(public settings:Settings) {
     for (let i = 0; i < 12; i++) {
+      //bottoni
+      if (i < 3) {
+        this.valori.push({mostra:false, testo:"" + (i+1)});
+      } else {
+        this.valori.push({mostra:true, testo:"" + (i+1)});
+      }
+
+      //caselle
       for (let ii = 0; ii < 12; ii++) {
         if (i < 3 && ii < 3)
           this.caselle.push({mostra:true, testo:"-", bckColor:""});
@@ -21,15 +31,31 @@ export class SinglePlayerComponent {
   }
 
   click(id:number) {
-    /* if(vinto) {
-        this.settings.aumentoLvl();
-        if (this.settings.livello < 11) this.mostraCaselle();
-      }
+    //controllo righe
+    for (let i = 0; i < (this.settings.livello + 2); i++) {
+      
+    }
+
+    //controllo colonne
+    for (let i = 0; i < (this.settings.livello + 2); i++) {
+
+    }
+
+    /*
+      this.settings.aumentoLvl();
+      if (this.settings.livello < 11) this.mostraCaselle();
     */
+    
   }
 
   mostraCaselle() {
     for (let i = 0; i < 12; i++) {
+      //bottoni
+      if (i < (this.settings.livello + 2)) {
+        this.valori[i].mostra = false;
+      }
+
+      //caselle
       for (let ii = i * 12; ii < (i + 1) * 12; ii++) {
         if (i < (this.settings.livello + 2) && ii < (i*12 + (this.settings.livello + 2))) {
           this.caselle[ii].mostra = true;
